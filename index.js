a35848732938f11366338f8eb6fa9eb98fec7719
Igor Burykin <iburykin071@gmail.com>
2023-11-21 10:37:33 +0200

    uibuild

--- index.js
+++ index.js
@@ -11,33 +11,10 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 app.use(express.static('dist'))
 
+
+
+
 const Person = require('./models/person')
-
-
-
-// let persons = [
-//     {
-//         "id": 1,
-//         "name": "Arto Hellas",
-//         "number": "040-123456"
-//     },
-//     {
-//         "id": 2,
-//         "name": "Ada Lovelace",
-//         "number": "39-44-5323523"
-//     },
-//     {
-//         "id": 3,
-//         "name": "Dan Abramov",
-//         "number": "12-43-234345"
-//     },
-//     {
-//         "id": 4,
-//         "name": "Mary Poppendieck",
-//         "number": "39-23-6423122"
-//     }
-// ]
-
 
 
 app.get('/api/persons', (request, response) => {
@@ -46,15 +23,13 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/info', (request, response) => {
-    response.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
-        <p>${new Date()}</p>`)
-})
+// app.get('/api/info', (request, response) => {
+//     response.send(
+//         `<p>Phonebook has info for ${persons.length} people</p>
+//         <p>${new Date()}</p>`)
+// })
 
 app.get('/api/persons/:id', (request, response) => {
-
-    const id = Number(request.params.id)
 
     Person.findById(request.params.id)
         .then(person => response.json(person))
@@ -67,43 +42,30 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
 
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+    Person.deleteOne({ _id: request.params.id })
+        .then(result => {
+            console.log(result)
+            response.status(204).end()
+        })
+        .catch(error => {
+            response.status(404).end()
+        })
 })
 
 app.post('/api/persons', (request, response) => {
 
-    const newPerson = request.body
-
-    if (!newPerson) {
-        return response.status(400).json({
-            error: 'error: content missing'
-        })
+    if (!request.body || !request.body.number) {
+        return response.status(400).json({ error: 'content missing' })
     }
 
-    if (!newPerson.name || !newPerson.number) {
-        return response.status(400).json({
-            error: 'error: name or number is missing'
-        })
-    }
+    const newPerson = new Person({
+        name: request.body.name,
+        phone: request.body.number
+    })
 
-    if (persons.filter(person => person.name.toLowerCase() === newPerson.name.toLowerCase()).length > 0) {
-        return response.status(400).json({
-            error: 'error: name must be unique'
-        })
-    }
-
-    const id = Math.floor(Math.random() * 1000000)
-    console.log(`random id generated: ${id}`)
-    newPerson.id = id
-
-    persons = persons.concat(request.body)
-
-    response.json(request.body)
+    newPerson.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
 
