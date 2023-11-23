d604af8a0cb8351f79b845d08964e1fc778a3791
Igor071 <iburykin071@gmail.com>
2023-11-23 19:52:50 +0200

    3.21 Deploying the database backend to production

--- models/person.js
+++ models/person.js
@@ -22,8 +22,14 @@ const personSchema = new mongoose.Schema({
     },
     number: {
         type: String,
-        minLength: 3,
-        required: true
+        minLength: 8,
+        validate: {
+            validator: function (v) {
+                return /\d{2,3}-\d{5}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
     },
 })
 
